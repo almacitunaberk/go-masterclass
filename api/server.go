75f16ecd6782c7f1c71e4468165ceb4c9cb0610a
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-08-01 09:47:25 +0300

    Added account routes

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	db "github.com/almacitunaberk/go_masterclass/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+
+	// Add routes to this Router
+	router.POST("/accounts", server.CreateAccount)
+	router.GET("/accounts/:id", server.GetAccount)
+	router.GET("/accounts", server.ListAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
